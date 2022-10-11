f4bb2cc19cc99b4cc461c12fa252a9726cfb199e
Yurii Popleta <yurka279@gmail.com>
2022-10-11 21:11:43 +0200

    Initial Commit

---
+++ Tipsy-App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Tipsy-App
+//
+//  Created by Admin on 11/10/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
